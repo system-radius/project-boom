509a04a514c9462488de62e86b61c309a287952c
system-radius <radius.sairo@gmail.com>
2024-02-07 18:03:50 +0800

    Disable debug view

--- core/src/com/radius/system/screens/GameScreen.java
+++ core/src/com/radius/system/screens/GameScreen.java
@@ -61,7 +61,7 @@ public class GameScreen extends AbstractScreen {
         InitializeStage();
         InitializeGameState();
 
-        debug = true;
+        debug = false;
     }
 
     private void InitializeStage() {
