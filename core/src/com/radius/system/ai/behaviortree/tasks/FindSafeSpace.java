dc27e42e05d4cb4196b88f333166a2ce853177d4
system-radius <radius.sairo@gmail.com>
2024-02-20 07:23:58 +0800

    Enable board cost solidification

--- core/src/com/radius/system/ai/behaviortree/tasks/FindSafeSpace.java
+++ core/src/com/radius/system/ai/behaviortree/tasks/FindSafeSpace.java
@@ -29,6 +29,7 @@ public class FindSafeSpace extends Solidifier {
 
     @Override
     public NodeState Evaluate(int depth, float delta, int[][] boardCost) {
+        super.Evaluate(depth, delta, boardCost);
 
         srcPoint = (Point) GetData(NodeKeys.SOURCE_POINT);
         if (srcPoint == null) {
