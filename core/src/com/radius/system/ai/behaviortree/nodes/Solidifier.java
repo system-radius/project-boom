5c686d04a9a197ecdae01406bc3aa740d89ffaf3
system-radius <radius.sairo@gmail.com>
2024-03-04 23:49:23 +0800

    Create a copy of the board cost to be altered

--- core/src/com/radius/system/ai/behaviortree/nodes/Solidifier.java
+++ core/src/com/radius/system/ai/behaviortree/nodes/Solidifier.java
@@ -4,18 +4,23 @@ import com.radius.system.ai.behaviortree.NodeKeys;
 import com.radius.system.ai.behaviortree.tasks.FindTarget;
 import com.radius.system.ai.pathfinding.Point;
 import com.radius.system.enums.NodeState;
+import com.radius.system.screens.game_ui.TimerDisplay;
 
 public abstract class Solidifier extends FindTarget {
 
     protected final int fireThreshold;
 
+    protected int[][] solidifiedBoard;
+
     public Solidifier(int fireThreshold) {
         this.fireThreshold = fireThreshold;
     }
 
+
     @Override
     public NodeState Evaluate(Point srcPoint, int[][] boardCost) {
         Boolean onFirePath = (Boolean) GetRoot().GetData(NodeKeys.ON_FIRE_PATH);
+        RefreshInternalBoardCost(boardCost);
         if (onFirePath == null || !onFirePath) {
             Solidify(boardCost);
         }
@@ -27,14 +32,25 @@ public abstract class Solidifier extends FindTarget {
         return this.Solidify(boardCost, fireThreshold);
     }
 
+    protected final void RefreshInternalBoardCost(int[][] boardCost) {
+        if (solidifiedBoard == null || solidifiedBoard.length != boardCost.length || solidifiedBoard[0].length != boardCost[0].length) {
+            solidifiedBoard = new int[boardCost.length][boardCost[0].length];
+        }
+
+        for (int i = 0; i < boardCost.length; i++) {
+            System.arraycopy(boardCost[i], 0,  solidifiedBoard[i], 0, boardCost[i].length);
+        }
+    }
+
     protected final int[][] Solidify(int[][] boardCost, int fireThreshold) {
+        RefreshInternalBoardCost(boardCost);
         for (int i = 0; i < boardCost.length; i++) {
             for (int j = 0; j < boardCost[i].length; j++) {
-                boardCost[i][j] = boardCost[i][j] > fireThreshold ? -1 : boardCost[i][j];
+                solidifiedBoard[i][j] = boardCost[i][j] > fireThreshold ? -1 : boardCost[i][j];
             }
         }
 
-        return boardCost;
+        return solidifiedBoard;
     }
 
 }
